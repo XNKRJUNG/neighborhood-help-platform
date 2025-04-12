936cdecbc462301281b685aee7679e79a0221dd9
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2025-04-12 00:07:26 -0500

    Inital project setup

---
+++ src/main/java/site/shresthacyrus/neighborhoodhelpplatform/NeighborhoodHelpPlatformApplication.java
@@ -0,0 +1,13 @@
+package site.shresthacyrus.neighborhoodhelpplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborhoodHelpPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeighborhoodHelpPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/shresthacyrus/neighborhoodhelpplatform/NeighborhoodHelpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shresthacyrus.neighborhoodhelpplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborhoodHelpPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
